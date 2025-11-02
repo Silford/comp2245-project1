0f7699d82caece8534ec2904fa8869ba60a9de09
Silford <silford.moore@my.fiveislands.uwi.edu>
2025-11-02 12:02:09 -0400

    Completed JS; finished project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".newsletter form");
+  const emailInput = document.querySelector("#email");
+  const messageDiv = document.querySelector(".message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = emailInput.value.trim();
+
+    if (email === "") {
+      messageDiv.innerHTML =
+        "<p><b>Please enter a valid email address.</b></p>";
+    } else {
+      messageDiv.innerHTML = `<p><b>Thank you! Your email address ${email} has been added to our mailing list!</b></p>`;
+    }
+  });
+});
